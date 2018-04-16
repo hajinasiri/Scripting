3ffcb129febe4e1d7058f4f06dd9e47045aff2de
vagrant <hajinasiri@gmail.com>
2018-04-16 01:05:27 +0000

    step5.js

---
+++ step5.js
@@ -0,0 +1,17 @@
+module.exports = function getHTML (options,callback) {
+  var https = require('https');
+  https.get(requestOptions, function (response){
+    response.setEncoding('utf8');
+    response.on('data',function (data) {callback(data)});
+  });
+}
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+function printHTML (html) {
+  console.log(html);
+};
+
